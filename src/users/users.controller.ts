60ebc470b1cc51ea85b43ed757187f1e71530b54
=basantaisg <basantaparajuli.11@gmail.com>
2025-02-18 13:59:46 +0545

    user-creation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+  // ?role=Value
+  @Get()
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return this.usersService.findAll(role);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() user: CreateUserDto) {
+    this.usersService.create(user);
+    return user;
+  }
+
+  @Patch(':id')
+  update(@Body() newUser: UpdateUserDto, @Param('id') id: string) {
+    return this.usersService.update(id, newUser);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.usersService.delete(id);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,79 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    {
+      id: uuid(),
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      role: 'INTERN',
+    },
+    {
+      id: uuid(),
+      name: 'Roma Doe',
+      email: 'romadoe@gmail.com',
+      role: 'ADMIN',
+    },
+    {
+      id: uuid(),
+      name: 'Baka Doe',
+      email: 'bakadoe@gmail.com',
+      role: 'ENGINEER',
+    },
+    {
+      id: uuid(),
+      name: 'Bakasur',
+      email: 'bakasur@gmail.com',
+      role: 'ENGINEER',
+    },
+    {
+      id: uuid(),
+      name: 'Randi Doe',
+      email: 'randidoe@gmail.com',
+      role: 'INTERN',
+    },
+    {
+      id: uuid(),
+      name: 'Suman Doe',
+      email: 'sumandoe@gmail.com',
+      role: 'INTERN',
+    },
+  ];
+
+  findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    if (role) {
+      return this.users.filter((user) => user.role === role);
+    }
+    return this.users;
+  }
+
+  findOne(id: string) {
+    return this.users.find((user) => user.id === id);
+  }
+
+  create(createUserDto: CreateUserDto) {
+    let id = uuid();
+    const newUser = { id, ...createUserDto };
+    return this.users.push(newUser);
+  }
+
+  update(id: string, updateUser: UpdateUserDto) {
+    const user = this.users.find((user) => user.id === id);
+    if (user) {
+      return Object.assign(user, updateUser);
+    }
+    throw new Error('User not found!');
+  }
+
+  delete(id: string) {
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    if (userIndex >= 0) {
+      return this.users.splice(userIndex, 1);
+    }
+    return new HttpException('User not found!', 404);
+  }
+}
